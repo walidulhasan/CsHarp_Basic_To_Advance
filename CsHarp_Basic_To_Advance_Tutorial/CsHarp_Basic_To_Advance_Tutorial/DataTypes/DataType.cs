2e0a77a1b2c6a42f408c5e993780e77f289b82cc
walidulhasan <walidulhasanboniamin@gmail.com>
2022-10-05 21:33:47 +0600

    add

---
+++ CsHarp_Basic_To_Advance_Tutorial/CsHarp_Basic_To_Advance_Tutorial/DataTypes/DataType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsHarp_Basic_To_Advance_Tutorial
+{
+    public class DataType
+    {
+       public string print = "Hello World";
+       public int number = 10;
+       public float floatNumber = 10.4f;
+       public double doubleNumber = 10.40;
+       public decimal decimalNumber = 30.45m;
+       public bool isTrue = false;
+       //public byte thisByte = 2223241242;
+
+
+    }
+}
--- CsHarp_Basic_To_Advance_Tutorial/CsHarp_Basic_To_Advance_Tutorial/Program.cs
+++ CsHarp_Basic_To_Advance_Tutorial/CsHarp_Basic_To_Advance_Tutorial/Program.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using CsHarp_Basic_To_Advance_Tutorial.TypeConversion;
+using CsHarp_Basic_To_Advance_Tutorial.ValueOrReferenceTypes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,15 +13,37 @@ namespace CsHarp_Basic_To_Advance_Tutorial
     {
         static void Main(string[] args)
         {
-            string print = "Hello World";
-            int number = 10;
-            float floatNumber = 10.4f;
-            double doubleNumber = 10.40;
-            decimal decimalNumber = 30.45m;
-            bool isTrue = false;
-            //byte thisByte = 2223241242;
+            #region DataType
+            //var dataType = new DataType();
+            //Console.WriteLine($"String: {dataType.print}\n{dataType.number}{dataType.floatNumber}{dataType.doubleNumber}{dataType.decimalNumber}{dataType.isTrue}{typeof(int)}");
 
-            Console.WriteLine($"String: {print}\n{number}{floatNumber}{doubleNumber}{decimalNumber}{isTrue}{typeof(int)}");
+            #endregion
+
+            #region Value Or Reference Type
+            //var obj = new ValueOrReferenceType();
+
+            //var valueType = obj.a = obj.b;
+            //var referenceType = obj.fName = obj.LName;
+            //Console.WriteLine($"\n valueType: {valueType}\n ReferenceType: {referenceType}");
+            //Console.WriteLine($"\n valueType: {obj.a}-{obj.b}\n ReferenceType: {obj.fName}-{obj.LName}");
+            #endregion
+
+            #region Type Conversion 
+            ////var obj1 = new typeconversion();
+            ////double implicitConversion = obj1.iNumber;
+            ////int explicitConversion = (int)obj1.dNumber;
+
+            ////Console.WriteLine($"\n Implicit:{implicitConversion}\n Explicit:{explicitConversion}");
+            #endregion
+
+            #region constants
+            //const int constants = 10;
+            //int constantsV = 10;
+            //constants = 10;
+            //constantsV = 10;
+            //Console.WriteLine(constants);
+            #endregion
+
             Console.ReadLine();
         }
     }
