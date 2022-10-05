5a395b93b7fb15985a99192764ded1e07b03a9ed
walidulhasan <walidulhasanboniamin@gmail.com>
2022-10-05 20:18:22 +0600

    initialComment

---
+++ CsHarp_Basic_To_Advance_Tutorial/CsHarp_Basic_To_Advance_Tutorial/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsHarp_Basic_To_Advance_Tutorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string print = "Hello World";
+            int number = 10;
+            float floatNumber = 10.4f;
+            double doubleNumber = 10.40;
+            decimal decimalNumber = 30.45m;
+            bool isTrue = false;
+            //byte thisByte = 2223241242;
+
+            Console.WriteLine($"String: {print}\n{number}{floatNumber}{doubleNumber}{decimalNumber}{isTrue}{typeof(int)}");
+            Console.ReadLine();
+        }
+    }
+}
